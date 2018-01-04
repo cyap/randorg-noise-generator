6d9d572d4de11b81713ef0ef86087bfa0f7832cf
Chester Yap <chesterdyap@gmail.com>
2018-01-03 22:37:43 -0600

    clean up functions

--- image.py
+++ image.py
@@ -1,13 +1,12 @@
 from PIL import Image
 
-def generate_bitmap(x=128, y=128, rand_nums=[1 for i in range(128 * 128)]):
+def generate_bitmap_from_array(arr, x, y):
 	image = Image.new('1', (x, y))
 	pixel_map = image.load()
 	for i in range(x * y):
-		pixel_map[int(i / x), i % y] = rand_nums[i]
+		pixel_map[int(i / x), i % y] = arr[i]
 	return image
 
-
-image = generate_bitmap()
-image.show()
-image.save("result.bmp")
+def display_and_save(image, name="result.bmp"):
+	image.show()
+	image.save(name)
