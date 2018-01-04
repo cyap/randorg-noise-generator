2e508c0964ad60056e027219dceea8aebccc41ea
Chester Yap <chesterdyap@gmail.com>
2018-01-03 22:49:01 -0600

    final cleanup

--- generate_random.py
+++ generate_random.py
@@ -1,13 +1,14 @@
 import image
 import request
 
-
+# Should actually be in a config file
 API_KEY = "00000000-0000-0000-0000-000000000000"
 LIMIT = 10000
 
 def generate_random_bitmap(x, y):
 	n = x * y
 	rand_arr = []
+
 	while n > 0:
 		requested_numbers = min(LIMIT, n)
 		n -= requested_numbers
@@ -19,6 +20,7 @@ def generate_random_bitmap(x, y):
 			'max': 1,
 			'replacement':"true"
 		}
+		
 		try:
 			response = request.make_randorg_request("generateIntegers", params)
 			rand_arr.extend(response['result']['random']['data'])
