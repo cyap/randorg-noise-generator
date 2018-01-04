99d40095f5f70e556d916ebfe0c7b1a674c0366a
Chester Yap <chesterdyap@gmail.com>
2018-01-03 22:36:10 -0600

    move information about rand array to main

--- request.py
+++ request.py
@@ -1,20 +1,14 @@
 import requests
 import json
 
-DATA = {
-	'jsonrpc': "2.0",
-	'method': "generateIntegers",
-	'params': {
-		'apiKey': "00000000-0000-0000-0000-000000000000",
-		'n': 16384,
-		'min': 0,
-		'max': 1,
-		'replacement': true,
-	},
-	'id': 0
-}
-def make_request(url="https://api.random.org/json-rpc/1/invoke"):
-	request = requests.get(url, data=json.dumps(DATA), headers=HEADERS)
-	return request.text
+URL = "https://api.random.org/json-rpc/1/invoke"
 
-#make_request()
+def make_randorg_request(method, params, url=URL):
+	data = {
+		'jsonrpc': "2.0",
+		'method': method,
+		'params': params,
+		'id': 0
+	}
+	request = requests.get(url, data=json.dumps(data), headers=HEADERS)
+	return json.loads(request.text)
